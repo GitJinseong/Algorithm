140412f73c9f2c89d00517810e977c6ecaf631ce
최진성 <134994862+GitJinseong@users.noreply.github.com>
2023-06-05 15:06:34 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    if (0 < num1 && num1 <= 100)
+    {
+    }
+    else if (0 < num2 && num2 <= 100)
+    {  
+    }
+    else
+    {
+        return 0;
+    }
+    int answer = num1 / num2;
+    return answer;
+}
